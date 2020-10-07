75c5da24ef46c8ba39725f9ec0b29bd551d07cce
Garry Polley <garry.polley@c2fo.com>
2020-10-07 12:15:56 -0500

    Update some of the layotu and componnets

--- site/src/components/banner.js
+++ site/src/components/banner.js
@@ -1,11 +1,11 @@
-import React from "react"
-export default function Banner() {
+import React from "react";
+import { Link } from "gatsby";
+
+export default function Banner({ title = "" }) {
   return (
     <div className="bg-blue-500 text-white p-4 text-center">
       <blockquote>
-      
-        Tickets are on sale now for X Conf! 
-       
+        <Link to="/ticket">Tickets are on sale now for {title}</Link>
       </blockquote>
     </div>
   );
